c4d7fd257b8667410aadf527698675954d5a0032
purelifexx1 <htt.contact167@gmail.com>
2022-06-14 22:29:56 +0700

    update test FOTA

--- .github/update_fw.sh
+++ .github/update_fw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ../../../ESP_PROJECT/ESP32/FOTA_test/build/FOTA_test.bin ./images/FOTA.bin
+cp ../../../ESP_PROJECT/ESP32/FOTA_test/build/FOTA_test.bin ./images/FOTA1.bin
 git add .
 git commit -m "update test FOTA"
 git push origin master
