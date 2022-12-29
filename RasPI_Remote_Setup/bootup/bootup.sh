e40453c80a91bf338930d2aeebbf35698c1e9a59
purelifexx1 <htt.contact167@gmail.com>
2022-12-29 15:19:19 +0000

    Update autossh service and script

--- RasPI_Remote_Setup/bootup/bootup.sh
+++ RasPI_Remote_Setup/bootup/bootup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-
 # sleep 20 second to make sure everything is up
-sleep 20
 HOSTNAME_FILE_LOCATION="/home/pi/workspace/init_script/bootup/e2c_host_name.txt"
 KEY_PAIR_LOCATION="/home/pi/workspace/init_script/bootup/vm_keypair.pem"
 
-while true
-do
-    # echo "$HOSTNAME_FILE_LOCATION" >> /home/pi/workspace/init_script/test.txt
-    E2C_HOSTNAME=$(cat $HOSTNAME_FILE_LOCATION)
-    ssh -i $KEY_PAIR_LOCATION -tt -o StrictHostKeyChecking=no -R 2222:localhost:22 ubuntu@$E2C_HOSTNAME
-    sleep 5
-done
+ssh -i $KEY_PAIR_LOCATION -tt -o ConnectTimeout=10 -o StrictHostKeyChecking=no -R 2222:localhost:22 ubuntu@$E2C_HOSTNAME
--- RasPI_Remote_Setup/bootup/bootup_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/pi/workspace/init_script/bootup/bootup.sh
