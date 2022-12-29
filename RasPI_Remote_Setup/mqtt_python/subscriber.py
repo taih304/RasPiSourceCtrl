a313db950b66fc433d067b7fe1f9e8e41e70607a
purelifexx1 <htt.contact167@gmail.com>
2022-12-29 17:34:49 +0000

    Some minor update in subscribe.py

--- RasPI_Remote_Setup/mqtt_python/subscriber.py
+++ RasPI_Remote_Setup/mqtt_python/subscriber.py
@@ -29,12 +29,11 @@ def exec(command):
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            debug("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            debug(f"Failed to connect, return code {rc}")
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
