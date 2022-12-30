53bc7a02c3160384cd4e7e9b9bdbc04cf7fe0d01
purelifexx1 <htt.contact167@gmail.com>
2022-12-30 18:29:15 +0000

    Fix when get status of the autossh service
    - When the autossh service is stop, the service no longer appear in
      service list, check the line is empty or not

--- RasPI_Remote_Setup/mqtt_python/get_autossh_status.sh
+++ RasPI_Remote_Setup/mqtt_python/get_autossh_status.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-autossh_status_line=$(systemctl --type=service | grep autossh.service)
+autossh_status_line=$(systemctl --type=service | grep -m 1 autossh.service)
 
-LOAD=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f3)
-ACTIVE=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f4)
-SUB=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f5)
+if [[ -z $autossh_status_line ]]; then
+    LOAD="none"
+    ACTIVE="none"
+    SUB="none"
+else
+    LOAD=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f3)
+    ACTIVE=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f4)
+    SUB=$(echo "$autossh_status_line" | tr -s ' ' | cut -d ' ' -f5)
+fi
+
 JSON_STR=$(jq --null-input --arg load $LOAD --arg active $ACTIVE --arg sub $SUB '{"load": $load, "active": $active, "sub": $sub}')
 
 echo "$JSON_STR"
