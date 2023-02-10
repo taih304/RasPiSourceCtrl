76c45a47e703120da5d79b3d0563710bc1958cce
purelifexx1 <htt.contact167@gmail.com>
2023-02-10 14:56:38 +0700

    Move all the configuration to /etc/my_service

--- RasPI_Remote_Setup/ssh_service/autossh.sh
+++ RasPI_Remote_Setup/ssh_service/autossh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CONF_FILE_LOCATION="~/.autossh.conf"
+CONF_FILE_LOCATION="/etc/my_service/.autossh.conf"
 
 #if [[ -h "$CONF_FILE_LOCATION" ]]; then
 if true; then
 
-KEY_PAIR_LOCATION=$(cat ~/.autossh.conf | jq -r .KEY_PAIR_LOCATION)
-E2C_HOSTNAME=$(cat ~/.autossh.conf | jq -r .E2C_HOSTNAME)
-SSH_PORT=$(cat ~/.autossh.conf | jq -r .PORT)
+KEY_PAIR_LOCATION=$(cat $CONF_FILE_LOCATION | jq -r .KEY_PAIR_LOCATION)
+E2C_HOSTNAME=$(cat $CONF_FILE_LOCATION | jq -r .E2C_HOSTNAME)
+SSH_PORT=$(cat $CONF_FILE_LOCATION | jq -r .PORT)
 
 ssh -i $KEY_PAIR_LOCATION -tt -o ConnectTimeout=10 -o StrictHostKeyChecking=no -R $SSH_PORT:localhost:22 ubuntu@$E2C_HOSTNAME
 
