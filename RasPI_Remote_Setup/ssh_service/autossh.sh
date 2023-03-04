ced33968afdf95524de75d3e33dba5ec4a318bc4
purelifexx1 <htt.contact167@gmail.com>
2023-03-04 23:32:21 +0700

    Update autossh service
    - Remove timeout option

--- RasPI_Remote_Setup/ssh_service/autossh.sh
+++ RasPI_Remote_Setup/ssh_service/autossh.sh
@@ -9,7 +9,7 @@ KEY_PAIR_LOCATION=$(cat $CONF_FILE_LOCATION | jq -r .KEY_PAIR_LOCATION)
 E2C_HOSTNAME=$(cat $CONF_FILE_LOCATION | jq -r .E2C_HOSTNAME)
 SSH_PORT=$(cat $CONF_FILE_LOCATION | jq -r .PORT)
 
-ssh -i $KEY_PAIR_LOCATION -tt -o ConnectTimeout=10 -o StrictHostKeyChecking=no -R $SSH_PORT:localhost:22 ubuntu@$E2C_HOSTNAME
+ssh -i $KEY_PAIR_LOCATION -tt -o StrictHostKeyChecking=no -R $SSH_PORT:localhost:22 ubuntu@$E2C_HOSTNAME
 
 else
 	echo "No conf file exist"
